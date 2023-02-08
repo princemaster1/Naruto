da64faf34cceda26d750498f80534562dd077ef2
Mohamed <123903854+spiderx11@users.noreply.github.com>
2023-02-08 20:11:58 +0300

    Add files via upload

--- commands/information/wikia.js
+++ commands/information/wikia.js
@@ -1,14 +1,14 @@
 const { fetchUrl } = require("../../lib/Function")
 
 module.exports = {
-    name: "blogger",
-    alias: ["blogspot"],
+    name: "wikia",
+    alias: ["wikien"],
     use: "<query>",
-    desc: "Get User Profile Url From Blogspot",
+    desc: "Get Information From Wikipedia Abstract",
     type: "information",
     example: `%prefix%command <query>`,
     start: async(killua, m, { text }) => {
-        let fetch = await fetchUrl(global.api("zenz", "/information/blogger", { query: text }, "apikey"))
+        let fetch = await fetchUrl(global.api("zenz", "/information/wikia", { query: text }, "apikey"))
         killua.sendText(m.from, fetch.result, m)
     },
     isQuery: true
