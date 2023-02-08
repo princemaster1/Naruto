98a53fe82bef5a851b4e8a2c6a67a8f4ccd93dc4
Mohamed <123903854+spiderx11@users.noreply.github.com>
2023-02-08 19:47:18 +0300

    Add files via upload

--- commands/animeweb/kissmanga.js
+++ commands/animeweb/kissmanga.js
@@ -1,20 +1,19 @@
 const { fetchUrl } = require("../../lib/Function")
 
 module.exports = {
-    name: "animeplanet",
+    name: "kissmanga",
     alias: [],
     use: "<query>",
-    desc: "Search Anime From Animeplanet",
+    desc: "Search Anime From Kissmanga",
     type: "animeweb",
     example: `%prefix%command <query>`,
     start: async(killua, m, { text }) => {
-        let fetch = await fetchUrl(global.api("zenz", "/animeweb/animeplanet", { query: text }, "apikey"))
-        let caption = `Animeplanet Search :\n\n`
+        let fetch = await fetchUrl(global.api("zenz", "/animeweb/kissmanga", { query: text }, "apikey"))
+        let caption = `Kissmanga Search :\n\n`
         for (let i of fetch.result) {
             caption += `⭔ Title : ${i.manga_name}\n`
             caption += `⭔ Link : ${i.manga_url}\n\n`
         }
-        //killua.sendFile(m.from, fetch.result[0].manga_thumb, "", m, { caption }) yg gambarnya kena internet positif
         killua.sendText(m.from, caption, m)
     },
     isQuery: true
